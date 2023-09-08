afa6e57744072674f2f489a96e16f37233b13f55
kdrbipul <kdrbipul@gmail.com>
2023-09-09 02:03:51 +0600

    first commit

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ var address = "motejhil";
 // console.log(address);
 var student1 = "Sakib Khan";
 
-console.log(student1, address);
+// console.log(student1, address);
+var doYouLoveMe = true;
+console.log(typeof(doYouLoveMe), doYouLoveMe);
